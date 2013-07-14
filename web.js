15428563d329ddbfb64178494347f3b76db3db55
Ehab Taleb <ehab.taleb@gmail.com>
2013-07-14 20:45:03 +0000

    add index

--- web.js
+++ web.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+vatr fileName = 'index.html';
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+// This line opens the file as a readable stream
+  var readStream = fs.createReadStream(filename);
+  readStream.on('open', function () {
+    // This just pipes the read stream to the response object (which goes to the client)
+    readStream.pipe(response);
+  });
+
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
