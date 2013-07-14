280a6f61c1bf85907ae99968fd9f3a66c578f524
Ehab Taleb <ehab.taleb@gmail.com>
2013-07-14 20:53:38 +0000

    add index

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 // This line opens the file as a readable stream
-  var readStream = fs.createReadStream(filename);
+  var readStream = fs.createReadStream(fileName);
   readStream.on('open', function () {
     // This just pipes the read stream to the response object (which goes to the client)
     readStream.pipe(response);
